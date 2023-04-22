1534dd44ce7af452756efdcd9a959087b3c8466e
dahliasan <laninaesloco@gmail.com>
2023-04-22 23:02:48 +1000

    update dark theme

--- components/tooltipWrapper.tsx
+++ components/tooltipWrapper.tsx
@@ -14,7 +14,7 @@ const TooltipWrapper = ({
         <Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
         <Tooltip.Portal>
           <Tooltip.Content
-            className="select-none rounded-[4px] bg-yellow-300 px-2 py-1 text-[15px] text-xs leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
+            className="select-none rounded-[4px] bg-yellow-300 px-2 py-1 text-[15px] text-xs leading-none text-black shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
             sideOffset={5}
           >
             {tooltipContent}
--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,7 +2,7 @@ import { OpenAI } from "langchain/llms/openai";
 import { LLMChain, ChatVectorDBQAChain, loadQAChain } from "langchain/chains";
 import { SupabaseVectorStore } from "langchain/vectorstores";
 import { PromptTemplate } from "langchain/prompts";
-import { openai, openaiStream } from "./openai-client";
+import { openai } from "./openai-client";
 
 const CONDENSE_PROMPT =
   PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. If the follow up question is already a standalone question, just return the follow up question.
